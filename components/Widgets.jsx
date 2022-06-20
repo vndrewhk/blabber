58f2347b844ffe29ff0f2d94b4918648a9599131
Andrew Ma <andrewma@Andrews-MacBook-Pro.local>
2022-06-20 16:39:36 -0400

    finished widgets

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -1,7 +1,31 @@
+import { SearchCircleIcon, SearchIcon } from "@heroicons/react/outline";
 import React from "react";
+import Trending from "./Trending";
 
-function Widgets() {
-  return <div>Widgets</div>;
+function Widgets({ trendingResults, followResults }) {
+  return (
+    <div className="hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5">
+      <div className="sticky items-center top-0 rounded-full py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
+        <div className="flex items-center bg-[#202327] p-3 rounded-full relative">
+          <SearchIcon className="text-gray-500 h-5 w-5 z-50"></SearchIcon>
+          <input
+            type="text"
+            placeholder="Search Blabber"
+            className=" bg-transparent placeholder-gray-500 outline-none text-[#d9d9d9] absolute inset-0 pl-11 border-transparent w-full"
+          ></input>
+        </div>
+      </div>
+      <div className="mt-3 text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
+        <h4 className="font-bold text-xl px-4">What's Happening?</h4>
+        {trendingResults.map((result, index) => (
+          <Trending key={index} result={result} index={index}></Trending>
+        ))}
+        <button className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0]">
+          Show more
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Widgets;
